fa163832f3a2f122d2564652448f66ccf01b60a2
allon <swliujr>
2014-08-29 09:30:18 +0800

    update 20140829

--- config.py
+++ config.py
@@ -5,25 +5,25 @@ port = 27017
 
 [mysql]
 #locale env
-#host = 10.0.0.6
-##port = 3306
-#username = user
-#password = user_pass
-#dbname = sqite
+host = 10.0.0.26
+#port = 3306
+username = meet_user
+password = meet_user
+dbname = meet
 
 #online env
-host = 172.168.4.32
-#port = 3306
-username = user
-password = user_pass
-dbname = sqite
+# host = 172.168.4.32
+# port = 3306
+# username = user
+# password = user_pass
+# dbname = sqite
 
 [general]
 
 #locale env
-#dsclogbase = /home/allon/python/data/constant
-#dfclogbase = /home/allon/python/data/file
+dsclogbase = /home/allon/workspace/Data-analysis/constant
+dfclogbase = /home/allon/workspace/Data-analysis/file
 
 #online env
-dsclogbase = /home/work/clog/constant
-dfclogbase = /home/work/clog/file
+# dsclogbase = /home/work/clog/constant
+# dfclogbase = /home/work/clog/file
